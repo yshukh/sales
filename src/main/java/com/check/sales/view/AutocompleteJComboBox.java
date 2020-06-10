22431f4cca77e85fc455ab09ac33e48485bad240
yshukh1997@gmail.com <Ibiza200$>
2020-06-11 00:42:39 +0300

    Fixed bug when only 1 option is available, added TODOs. Pom version updated

--- src/main/java/com/check/sales/view/AutocompleteJComboBox.java
+++ src/main/java/com/check/sales/view/AutocompleteJComboBox.java
@@ -10,15 +10,27 @@ import java.awt.*;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class AutocompleteJComboBox extends JComboBox<InventoryItemDto> {
 
-    private final Searchable<InventoryItemDto, String> optionsSource;
+    private final Searchable<InventoryItemDto, String> dropdownOptionsSource;
 
-    // TODO implement not copying when an option is selected, instead copy only when Enter pressed
+    // TODO handle when option selection has been confirmed via Enter (and think about another ways of selecting)
+
+    // TODO allow navigate through whole dropdown list without comboboxInput changing
+    //  (now it works as next: when user navigates through dropdown the comboboxInput changes immediately according to
+    //  value which is selected and dropdown gets reloaded by list containing only one value)
+
+    // TODO implement ignore case for inventory item name searching within comboboxInput
+
+    // TODO add barcode supporting and think how to handle both inventory item names and barcodes in combobox
+    //  (first idea to check: if comboboxInput contains only numbers, then it is barcode; else inventory item.
+    //  Consequently, inventory item name should contain at least one letter or non-number character)
     public AutocompleteJComboBox(Searchable<InventoryItemDto, String> optionsSource) {
         super();
-        this.optionsSource = optionsSource;
+        this.dropdownOptionsSource = optionsSource;
         initialize();
     }
 
@@ -52,17 +64,28 @@ public class AutocompleteJComboBox extends JComboBox<InventoryItemDto> {
                 update();
             }
 
-            public void update() {
+            private void update() {
                 SwingUtilities.invokeLater(() -> {
-                    Collection<InventoryItemDto> suitableOptions =
-                            self.optionsSource.search(comboboxInput.getText());
+                    final Collection<InventoryItemDto> suitableOptions =
+                            self.dropdownOptionsSource.search(comboboxInput.getText());
                     self.setEditable(false);
                     self.removeAllItems();
+                    handleCurrentInput(suitableOptions);
                     suitableOptions.forEach(self::addItem);
                     self.setPopupVisible(true);
                     self.setEditable(true);
                     comboboxInput.requestFocus();
                 });
+            }
+
+            private void handleCurrentInput(final Collection<InventoryItemDto> suitableOptions) {
+                List<String> dropdownTerms = suitableOptions.stream()
+                        .map(InventoryItemDto::getName)
+                        .collect(Collectors.toList());
+                // add only if input isn't present in dropdown
+                if (!dropdownTerms.contains(comboboxInput.getText())) {
+                    self.addItem(new InventoryItemDto(-1, comboboxInput.getText()));
+                }
             }
         });
     }
